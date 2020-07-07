44ff9000e2ca55901873778d8965f86faa946305
Ajith kumar <babulakshmi691@gmail.com>
2020-07-07 13:33:57 +0530

    gettodobyid

--- testAPI.js
+++ testAPI.js
@@ -2,9 +2,48 @@ var express = require('express');
 var app =  express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id : 1,
+	name : 'Ajith',
+	place : 'vridhachalam'
+},{
+	id : 2,
+	name : 'Akilan',
+	place : 'vridhachalam'
+}];
+
+
 
 app.get('/',function(req,res){
 res.send('API created...!');
+
+});
+
+app.get('/json',function(req,res){
+res.json(todos);
+
+});
+
+app.get('/json/:id',function(reg,res){
+	var todoid = parseInt(reg.params.id,10);
+	var matchedtodo;
+
+	todos.forEach(function (toto){
+
+if (todoid === toto.id) {
+
+	matchedtodo = toto;
+}
+	});
+
+if (matchedtodo) {
+	res.json(matchedtodo);
+}
+else{
+	res.status(404).send();
+}
+
+
 
 });
 
